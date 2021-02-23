cb681c46680c2315371d92dc0e9410cc7eafdb70
Anastasia <72605955+stacy1999@users.noreply.github.com>
2021-02-23 13:04:29 +0000

    Add files via upload

---
+++ Airport/spec/FeatureSpec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('Feature Test:', function(){
+  var plane;
+  var airport;
+
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  });
+  
+  it('planes can be instructed to land at an airport', function(){
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+  
+  it('planes can be instructed to take off from an airport', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+
+  
+
+
+
+});
---
+++ Airport/spec/airportSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Airport', function(){
+  var airport;
+  var plane;
+  beforeEach (function(){
+    airport = new Airport();
+    plane = jasmine.createSpy('plane', ['land']);
+  });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+  it('can clear for planes for landing', function(){
+      airport.clearForLanding(plane);
+      expect(airport.planes()).toEqual([plane]);
+  });
+  it('can clear planes for takeoff', function(){
+    airport.clearForLanding(plane);
+    airport.clearForTakeoff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
+});
